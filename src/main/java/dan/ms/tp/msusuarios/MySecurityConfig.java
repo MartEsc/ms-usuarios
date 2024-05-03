21fc80d663bf722dc9eb3a8038a7d01e5553e40b
HoracioGayosoFolder <hgayoso@folderit.net>
2024-05-03 00:30:02 -0300

    Keycloak funcionando...falta hacer el endpoint de Login, pero podemos hacerlo fuera de este api

--- src/main/java/dan/ms/tp/msusuarios/MySecurityConfig.java
+++ src/main/java/dan/ms/tp/msusuarios/MySecurityConfig.java
@@ -1,7 +1,11 @@
 package dan.ms.tp.msusuarios;
 
+import dan.ms.tp.msusuarios.config.JwtAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,15 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class MySecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationConverter jwtAuthenticationConverter;
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(http -> http.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> {
-                    oauth.jwt(jwt -> {});
+                    oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter));
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
--- src/main/java/dan/ms/tp/msusuarios/rest/controllers/HelloController.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/controllers/HelloController.java
@@ -1,15 +1,18 @@
 package dan.ms.tp.msusuarios.rest.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello-1")
+    @PreAuthorize("hasRole('administracion')")
     public String helloAdmin(){
         return "Hello Admin!";
     }
     @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('escritura') or hasRole('lectura')")
     public String helloUser(){
         return "Hello User!";
     }
